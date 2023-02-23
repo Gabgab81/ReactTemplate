d2ba6506541445ab48192c5ad4e6471bc0a2aafc
gabriel roux <rouxgab81@gmail.com>
2023-02-23 11:32:40 -0500

    change the structure folder

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import './app.scss';
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from '@fortawesome/free-brands-svg-icons';
